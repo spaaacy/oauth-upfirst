d06686e2a94f4ba5ffa2088dc2cff949667d2ba8
spaaacy <90762160+spaaacy@users.noreply.github.com>
2025-02-06 13:37:39 -0500

    Began authorize

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { SignJWT } from "jose";
 
 const app = express();
+// For URL encoded body
+app.use(express.urlencoded({ extended: true }));
 
 // Hard-coded to avoid using extra (crypto) deps
 const secret = "99aeee4d07fc8698eafe377a79b2c95e54b9f7a4d430d801fa133093d68eda60";
@@ -18,8 +20,6 @@ app.get("/api/oauth/authorize", async (req: Request, res: Response) => {
   const { response_type, client_id, redirect_uri } = req.query;
   const clientId = client_id as string;
 
-  console.log(clientId);
-
   if (!approvedApplications[clientId]) return res.status(400).json({ error: "Invalid client_id" });
   if (approvedApplications[clientId].redirectUri !== redirect_uri)
     return res.status(400).json({ error: "Invalid redirect_uri" });
@@ -35,7 +35,10 @@ app.get("/api/oauth/authorize", async (req: Request, res: Response) => {
   res.redirect(`${redirect_uri}?code=${jwt}`);
 });
 
-app.post("/api/oauth/token", () => {});
+app.post("/api/oauth/token", (req: Request, res: Response) => {
+  const { grant_type, code, client_id, redirect_uri } = req.body;
+  console.log({ grant_type, code, client_id, redirect_uri });
+});
 
 app.listen(8080, () => {
   console.log("Server started...");
